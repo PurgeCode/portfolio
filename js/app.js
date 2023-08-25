0a23d257251fa7588a7785bb344377d7b33d0399
Jury Gregorio Jr <jury.romblondev@outlook.com>
2023-08-25 21:12:17 +0800

    Update HTML CSS JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Selecting Elements
 const filter_btns = document.querySelectorAll(".filter-btn");
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
@@ -8,94 +9,99 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 
+// Footer Input/Subscribe input
 footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
+   footer_input.classList.add("focus");
 });
 
 footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
+   if (footer_input.value != "") return;
+   footer_input.classList.remove("focus");
 });
 
+// Navigation menu for responsive
 function closeMenu() {
-  navbar.classList.remove("open");
-  document.body.classList.remove("stop-scrolling");
+   navbar.classList.remove("open");
+   document.body.classList.remove("stop-scrolling");
 }
 
 hamburger_menu.addEventListener("click", () => {
-  if (!navbar.classList.contains("open")) {
-    navbar.classList.add("open");
-    document.body.classList.add("stop-scrolling");
-  } else {
-    closeMenu();
-  }
+   if (!navbar.classList.contains("open")) {
+      navbar.classList.add("open");
+      document.body.classList.add("stop-scrolling");
+   } else {
+      closeMenu();
+   }
 });
 
 links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
+// Filter in Portfolio this connected with the package isotope
 filter_btns.forEach((btn) =>
-  btn.addEventListener("click", () => {
-    filter_btns.forEach((button) => button.classList.remove("active"));
-    btn.classList.add("active");
+   btn.addEventListener("click", () => {
+      filter_btns.forEach((button) => button.classList.remove("active"));
+      btn.classList.add("active");
 
-    let filterValue = btn.dataset.filter;
+      let filterValue = btn.dataset.filter;
 
-    $(".grid").isotope({ filter: filterValue });
-  })
+      $(".grid").isotope({ filter: filterValue });
+   })
 );
 
 $(".grid").isotope({
-  itemSelector: ".grid-item",
-  layoutMode: "fitRows",
-  transitionDuration: "0.6s",
+   itemSelector: ".grid-item",
+   layoutMode: "fitRows",
+   transitionDuration: "0.6s",
 });
 
+// Skill set's
 window.addEventListener("scroll", () => {
-  skillsEffect();
-  countUp();
+   skillsEffect();
+   countUp();
 });
 
 function checkScroll(el) {
-  let rect = el.getBoundingClientRect();
-  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
-  return false;
+   let rect = el.getBoundingClientRect();
+   if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+   return false;
 }
 
 function skillsEffect() {
-  if (!checkScroll(skills_wrap)) return;
-  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+   if (!checkScroll(skills_wrap)) return;
+   skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
 }
 
 function countUp() {
-  if (!checkScroll(records_wrap)) return;
-  records_numbers.forEach((numb) => {
-    const updateCount = () => {
-      let currentNum = +numb.innerText;
-      let maxNum = +numb.dataset.num;
-      let speed = 100;
-      const increment = Math.ceil(maxNum / speed);
+   if (!checkScroll(records_wrap)) return;
+   records_numbers.forEach((numb) => {
+      const updateCount = () => {
+         let currentNum = +numb.innerText;
+         let maxNum = +numb.dataset.num;
+         let speed = 100;
+         const increment = Math.ceil(maxNum / speed);
 
-      if (currentNum < maxNum) {
-        numb.innerText = currentNum + increment;
-        setTimeout(updateCount, 1);
-      } else {
-        numb.innerText = maxNum;
-      }
-    };
+         if (currentNum < maxNum) {
+            numb.innerText = currentNum + increment;
+            setTimeout(updateCount, 1);
+         } else {
+            numb.innerText = maxNum;
+         }
+      };
 
-    setTimeout(updateCount, 400);
-  });
+      setTimeout(updateCount, 400);
+   });
 }
 
+// Swiper for testimonials
 var mySwiper = new Swiper(".swiper-container", {
-  speed: 1100,
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    prevEl: ".swiper-button-prev",
-    nextEl: ".swiper-button-next",
-  },
+   speed: 1100,
+   slidesPerView: 1,
+   loop: true,
+   autoplay: {
+      delay: 5000,
+   },
+   navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+   },
 });
